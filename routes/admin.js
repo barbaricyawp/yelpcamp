9af7ff210feff3d50b142281a2126990c9590963
barbaricyawp <barbaricyawp24@gmail.com>
2017-11-21 02:48:36 +0000

    add restaurant list in admin section

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require("../models/user");
+var Restaurant = require("../models/restaurant");
 var middleware = require("../middleware");
 
 router.get("/", function(req, res){
@@ -14,6 +15,16 @@ router.get("/userlist", function(req, res){
             console.log(err);
         } else {
             res.render("admin/user_list", {user: allUsers});
+        }
+    });
+});
+
+router.get("/restaurantlist", function(req, res){
+    Restaurant.find({}, function(err, allRestaurants){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("admin/restaurant_list", {user: allRestaurants});
         }
     });
 });
